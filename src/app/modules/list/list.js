1e5bf26c253bbf5186a5a4fb8dca5d7f3d546084
王金宝 <wangjinbao@4zlink.com>
2016-11-18 18:28:31 +0800

    modify 脚手架

--- src/app/modules/list/list.js
+++ src/app/modules/list/list.js
@@ -3,8 +3,22 @@
 angular.module('canhe.list', [])
     .controller('mylist', ['$rootScope','$scope', function ($rootScope,$scope) {
         $scope.nowItem=1;
+        $scope.secondItem=1;
         $scope.sortList=[
             {name:'分类一',id:1},{name:'分类一',id:2},{name:'分类一',id:3},{name:'分类一',id:4},{name:'分类一',id:5},{name:'分类一',id:6},{name:'分类一',id:7},{name:'分类一',id:8}
+        ]
+
+        $scope.secondList=[
+            {name:'五格餐盒',id:1},{name:'四格餐盒',id:2},{name:'三格餐盒',id:3},{name:'二格餐盒',id:4},{name:'一格餐盒',id:5},{name:'分类一',id:6},{name:'分类一',id:7},{name:'分类一',id:9},{name:'分类一',id:8},{name:'分类一',id:10}
+        ]
+
+        $scope.proList=[
+            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'190.00',id:263,type:1},
+            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'120.00',id:263,type:1},
+            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'130.00',id:263,type:1},
+            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'98.00',id:263,type:1},
+            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'100.00',id:263,type:1},
+            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'35.00',id:263,type:1}
         ]
 
         $scope.changeSortItem=function(val){
@@ -14,11 +28,10 @@ angular.module('canhe.list', [])
             $scope.nowItem=val.id;
         }
 
-        $scope.addPro=function(){
-            $rootScope.buyNum++;
-        }
-
-        $scope.reducePro=function(){
-            $rootScope.buyNum--;
+        $scope.changeSecondItem=function(val){
+            if($scope.secondItem==val.id){
+                return;
+            }
+            $scope.secondItem=val.id;
         }
     }])
