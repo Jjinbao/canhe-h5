9c0a4b473ae4f253b425b72ece1b4846f87baecb
Jjinbao <jinbaow@yeah.net>
2016-11-29 22:56:30 +0800

    modify 脚手架

--- src/app/modules/list/list.js
+++ src/app/modules/list/list.js
@@ -1,42 +1,43 @@
 'use strict'
 
 angular.module('canhe.list', [])
-    .controller('mylist', ['$rootScope','$scope','$state','$ionicViewSwitcher', function ($rootScope,$scope,$state,$ionicViewSwitcher) {
-        $scope.nowItem=1;
-        $scope.secondItem=1;
-        $scope.sortList=[
-            {name:'分类一',id:1},{name:'分类一',id:2},{name:'分类一',id:3},{name:'分类一',id:4},{name:'分类一',id:5},{name:'分类一',id:6},{name:'分类一',id:7},{name:'分类一',id:8}
-        ]
+  .controller('mylist', ['$rootScope', '$scope', function ($rootScope, $scope) {
+    $scope.nowItem = 1;
+    $scope.secondItem = 1;
+    $scope.sortList = [
+      {name: '分类一', id: 1}, {name: '分类一', id: 2}, {name: '分类一', id: 3}, {name: '分类一', id: 4}, {
+        name: '分类一',
+        id: 5
+      }, {name: '分类一', id: 6}, {name: '分类一', id: 7}, {name: '分类一', id: 8}
+    ]
 
-        $scope.secondList=[
-            {name:'五格餐盒',id:1},{name:'四格餐盒',id:2},{name:'三格餐盒',id:3},{name:'二格餐盒',id:4},{name:'一格餐盒',id:5},{name:'分类一',id:6},{name:'分类一',id:7},{name:'分类一',id:9},{name:'分类一',id:8},{name:'分类一',id:10}
-        ]
+    $scope.secondList = [
+      {name: '五格餐盒', id: 1}, {name: '四格餐盒', id: 2}, {name: '三格餐盒', id: 3}, {name: '二格餐盒', id: 4}, {
+        name: '一格餐盒',
+        id: 5
+      }, {name: '分类一', id: 6}, {name: '分类一', id: 7}, {name: '分类一', id: 9}, {name: '分类一', id: 8}, {name: '分类一', id: 10}
+    ]
 
-        $scope.proList=[
-            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'190.00',id:263,type:1},
-            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'120.00',id:263,type:1},
-            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'130.00',id:263,type:1},
-            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'98.00',id:263,type:1},
-            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'100.00',id:263,type:1},
-            {name:'圆形一次性餐盒1000ML塑料透明圆碗外卖',price:'35.00',id:263,type:1}
-        ]
+    $scope.proList = [
+      {name: '圆形一次性餐盒1000ML塑料透明圆碗外卖', price: '190.00', id: 263, type: 1},
+      {name: '圆形一次性餐盒1000ML塑料透明圆碗外卖', price: '120.00', id: 263, type: 1},
+      {name: '圆形一次性餐盒1000ML塑料透明圆碗外卖', price: '130.00', id: 263, type: 1},
+      {name: '圆形一次性餐盒1000ML塑料透明圆碗外卖', price: '98.00', id: 263, type: 1},
+      {name: '圆形一次性餐盒1000ML塑料透明圆碗外卖', price: '100.00', id: 263, type: 1},
+      {name: '圆形一次性餐盒1000ML塑料透明圆碗外卖', price: '35.00', id: 263, type: 1}
+    ]
 
-        $scope.changeSortItem=function(val){
-            if($scope.nowItem==val.id){
-                return;
-            }
-            $scope.nowItem=val.id;
-        }
+    $scope.changeSortItem = function (val) {
+      if ($scope.nowItem == val.id) {
+        return;
+      }
+      $scope.nowItem = val.id;
+    }
 
-        $scope.changeSecondItem=function(val){
-            if($scope.secondItem==val.id){
-                return;
-            }
-            $scope.secondItem=val.id;
-        }
-
-        $scope.toDetail=function(){
-            $state.go('detail',{});
-            $ionicViewSwitcher.nextDirection('forward');
-        }
-    }])
+    $scope.changeSecondItem = function (val) {
+      if ($scope.secondItem == val.id) {
+        return;
+      }
+      $scope.secondItem = val.id;
+    }
+  }])
